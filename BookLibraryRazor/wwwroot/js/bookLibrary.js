c839c63b6792d8ddfc058c2ebf6d8a4bd3cd9d7f
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-11-14 15:51:42 +0300

    Upsert Page UI

--- BookLibraryRazor/wwwroot/js/bookLibrary.js
+++ BookLibraryRazor/wwwroot/js/bookLibrary.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <a href = "/BookLibrary/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        <a href = "/BookLibrary/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a >
                             &nbsp;
